6728e1b1cef080c101f497dd353b8b36729b36b4
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-03-28 23:37:13 +0530

    displaying content from database

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,3 +1,17 @@
+import React, {useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import {firestore} from '../firebase';
+
 export const PostDetail = ()=>{
-  return( <div>Post details</div>)
+  const [post, setpost] = useState({});
+  const {postId} = useParams();
+  useEffect(()=>{
+    firestore.collection("posts").doc(postId).get().then((snapshot)=>{
+      setpost(snapshot.data());
+    })
+  })
+  return( <div>
+            <h1>{post.title}</h1>
+            <p>{post.Content}</p>
+  </div>)
 }
